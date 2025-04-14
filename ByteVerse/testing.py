d0829448912863b9aec7ed1ee978231ce67f6b37
bhadauria-harish <bhadauriaharish93404@gmail.com>
2025-04-14 12:42:33 +0530

    Delete ByteVerse directory

--- ByteVerse/testing.py
+++
@@ -1,9 +0,0 @@
-from barcode_details import get_product_info
-
-# barcode = "5449000000996"  # Coca-Cola
-barcode = "8901058100600"  # Milkmaid
-# 8901058100600
-result = get_product_info(barcode)
-print(result)
-
-
